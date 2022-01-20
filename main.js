5a643e1d49aa5cef6473c53e59bf9b9a78cac891
Mattia Bardino <Mattia.errol@gmail.com>
2022-01-20 15:53:51 +0100

    Forse fatto

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 let Km = prompt ("Quanti Km devi percorrere?")
 
+let age = prompt ("Quanti anni hai?")
+
 let Km_price = Km * 0.21;
 console.log (Km_price);
+
+if (age < 18) {
+ let sale = (Km_price*20/100);
+ document.getElementById("calculator").innerHTML = ("il costo del biglietto è di " + (Km_price - sale) + " euro");
+
+} else if (age > 65) {
+ let sale = (Km_price *40/100);
+ document.getElementById("calculator").innerHTML = ("il costo del biglietto è di " + (Km_price - sale) + " euro");
+}
+
+
+
+
+
+
